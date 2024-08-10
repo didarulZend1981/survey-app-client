d0ae9d799029ddb20f75fafa70aed2a26e41ee63
didarulZend1981exitgit config --global user.name didarulZend1981git config --global user.name didarulZend1981 <didarul1981@gmail.com>
2024-08-10 21:33:14 +0600

    testing

--- client/src/pages/Home/Home/Banner/Banner.jsx
+++ client/src/pages/Home/Home/Banner/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-lg">
-      <h1 className="mb-5 text-5xl font-bold font-poppins">Survey App</h1>
+      <h1 className="mb-5 text-5xl font-bold font-poppins">Survey App test</h1>
       <p className="mb-5 font-poppins">By conducting survey on our application T Market products, I can see which products are currently in the market and which products will come to the market in the future. I tell our cast members after researching how long they can stay in the market. They do whatever they want.</p>
       <button className="btn bg-[#1da1f2] text-white hover:bg-[#3488bd]">overview and explore</button>
     </div>
